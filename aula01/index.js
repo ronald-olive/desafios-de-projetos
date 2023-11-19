01dd5dc5bb3379f418791ff3ff50ae0ae975ff3d
ron4ldblue <ronaldblue54@gmail.com>
2023-11-19 13:56:51 -0300

    desafio da dio classificador de heroi

---
+++ aula01/index.js
@@ -0,0 +1,24 @@
+let nome = "Ronaldo"
+let xp = 7001
+let nivel = ""
+
+if (xp <= 1000) {
+    nivel = "Ferro"
+} else if (xp <= 2000) {
+    nivel = "Bronze"
+} else if (xp <= 6000) {
+    nivel = "Prata"
+} else if (xp <= 7000) {
+    nivel = "Ouro"
+} else if (xp <= 8000) {
+    nivel = "Platina"
+} else if (xp <= 9000) {
+    nivel = "Ascendente"
+} else if (xp <= 10.000) {
+    nivel = "Imortal"
+} else if (xp >= 10001) {
+    nivel = "Radiante"
+}
+
+console.log(`O herói de nome ${nome} está no nível de ${nivel}`)
+
