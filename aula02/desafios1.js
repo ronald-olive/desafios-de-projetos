9127ea1e042a29b99764911f15e4e58a2bfab0fb
ron4ldblue <ronaldblue54@gmail.com>
2023-11-21 13:22:01 -0300

    desafio rank do heroi

---
+++ aula02/desafios1.js
@@ -0,0 +1,27 @@
+function matches(vitoria, derrota) {
+    let result = vitoria - derrota
+    return result 
+}
+
+function rank() {
+    let resultRank = matches(100, 10)
+    let nivel = ""
+    if (resultRank <= 10) {
+        nivel = "Ferro"
+    } else if (resultRank <= 20) {
+        nivel = "Bronze"
+    } else if (resultRank <= 50) {
+        nivel = "Prata"
+    } else if (resultRank <= 80) {
+        nivel = "Ouro"
+    } else if (resultRank <= 90) {
+        nivel = "Diamante"
+    } else if (resultRank <= 100) {
+        nivel = "Lendário"
+    } else if (resultRank >= 100) {
+        nivel = "Imortal"
+    }
+    return `O Herói tem  saldo de ${resultRank} e está no nível ${nivel}`
+}
+
+console.log(rank())
